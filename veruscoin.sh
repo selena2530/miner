f7f90ad03cf6feae88f3f945ad78664c9ac05a64
selena2530 <84287970+selena2530@users.noreply.github.com>
2021-05-17 16:17:48 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DM7YBYo9yzQ4PxbnCNHhjBTkpKeJv2pkAe.bricks -p c=DOGE -t 4
